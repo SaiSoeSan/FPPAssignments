fa7daebb95b5f83c499b532bd830c8b5f042ee51
SaiSoeSan <33777412+SaiSoeSan@users.noreply.github.com>
2024-05-27 17:08:19 -0500

    generate random number with randomgenerator

--- Prog1.java
+++ Prog1.java
@@ -1,12 +1,15 @@
-import java.util.Random;
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
 
 public class Prog1 {
     public static void main(String[] args) {
-        Random rand = new Random();
-        int randomNum = rand.nextInt((9 - 1) + 1) + 1;
+        // Create a RandomGenerator instance
+        RandomGenerator randomGenerator = RandomGeneratorFactory.of("L32X64MixRandom").create();
+        int randomNum = randomGenerator.nextInt(1, 9);
         double pi = Math.PI;
+
         System.out.printf("%.2f\n", Math.pow(pi,randomNum));
-        int randomNum2 = rand.nextInt((14 - 3) + 1) + 3;
+        int randomNum2 = randomGenerator.nextInt(3, 14);
         System.out.printf("%.2f", Math.pow(pi,randomNum2));
     }
 }
